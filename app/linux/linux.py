4fa4bb80a718314da06b9a964362961663ea4cf7
Hbisneto <hbarbisneto@gmail.com>
2023-10-24 22:36:08 -0300

    Added support to Linux and Windows

--- app/linux/linux.py
+++ app/linux/linux.py
@@ -1,35 +1,36 @@
 ﻿"""
 ### linux.py
 
-- This file is used to implement code used to run scripts for Mac
+- This file is used to implement code used to run scripts for Linux
 - Codes implemented here, will run before the main script starts running
 """
-## Mac File
-## This file is used to implement code used to run scripts for Mac
+## Linux File
+## This file is used to implement code used to run scripts for Linux
 ## Codes implemented here, will run before the main script starts running
 
-import app
 import info
-from system import filesystem as fs
+import filesystem as fs
+from filesystem import wrapper as wr
+from system import core
 from system import requirements as req
+import pybridge
 
 def linux():
    ## NOTE: You can use this function
    ## To load information before the app starts running
 
-   ## Lets get Application Info (APPINFO.py)
+   ## Lets get Application Info (info.py)
    info.loadSplashScreen()
 
    ## Lets check system requirements
    req.check_version()
 
    ### You just need to run ONCE: Be sure you commented this code after first run
-   # Requirements.InstallDependencies()
+   # req.install_dependencies()
    ### You just need to run ONCE: Be sure you commented this code after first run
 
    ### Creates all needed folders
-   fs.create_custom_folder(info.NAME, fs.documents)
-   fs.create_custom_folder("Repository", f'{fs.documents}/{info.NAME}')
+   wr.create_directory(f'{fs.documents}/{info.NAME}/Repository')
 
-   ## Start App for Mac
-   app.start()
+   ## Start App for Linux
+   pybridge.start()
--- app/windows/windows.py
+++ app/windows/windows.py
@@ -1,36 +1,36 @@
 ﻿"""
 ### windows.py
 
-- This file is used to implement code used to run scripts for Mac
+- This file is used to implement code used to run scripts for Windows
 - Codes implemented here, will run before the main script starts running
 """
-## Mac File
-## This file is used to implement code used to run scripts for Mac
+## Windows File
+## This file is used to implement code used to run scripts for Windows
 ## Codes implemented here, will run before the main script starts running
 
-import app
 import info
-from system import filesystem as fs
-from system import requirements as req
+import filesystem as fs
+from filesystem import wrapper as wr
 from system import core
+from system import requirements as req
+import pybridge
 
 def windows():
    ## NOTE: You can use this function
    ## To load information before the app starts running
 
-   ## Lets get Application Info (APPINFO.py)
+   ## Lets get Application Info (info.py)
    info.loadSplashScreen()
 
    ## Lets check system requirements
    req.check_version()
 
    ### You just need to run ONCE: Be sure you commented this code after first run
-   # Requirements.InstallDependencies()
+   # req.install_dependencies()
    ### You just need to run ONCE: Be sure you commented this code after first run
 
    ### Creates all needed folders
-   core.create_custom_folder(info.NAME, fs.documents)
-   core.create_custom_folder("Repository", f'{fs.documents}/{info.NAME}')
+   wr.create_directory(f'{fs.documents}/{info.NAME}/Repository')
 
-   ## Start App for Mac
-   app.start()
+   ## Start App for Windows
+   pybridge.start()
